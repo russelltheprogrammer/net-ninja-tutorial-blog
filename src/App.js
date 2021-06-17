579dee76a9c25d7aa051dc38a9147911627a839d
russelltheprogrammer <russelltheprogrammer@gmail.com>
2021-06-17 16:05:15 -0400

    Up to #10

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import './App.css';
+import Navbar from './Navbar'
+import Home from './Home'
+import './index.css';
 
 const App = () => {
 
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  // const person = { name: "yoshi", age: 30 };
-const link = "http://www.google.com";
+  
+
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-      <h1>{ title }</h1>
-      <p>Liked { likes } </p>
-      {/* <p> { person }</p> */}
 
-<p>{ 10 }</p>
-<p>{ "Hello, ninjas" }</p>
-<p>{ [1,2,3,4,5] } </p>
-<p> { Math.random() * 10 } </p>
-
-<a href={link}>Google Site</a>
+      
+      <Home />
       </div>
+  
     </div>
   );
 }
